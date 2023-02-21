ef48fca112e4fd982ebbdf73b88da3582a0564fc
Marchalek <marchalek@hotmail.com>
2023-02-20 23:05:27 -0300

    implementação manual de encapsulamento

--- Course mod5/Produto.cs
+++ Course mod5/Produto.cs
@@ -1,41 +1,60 @@
 ﻿using System.Globalization;
 namespace Course_mod5 {
     class Produto {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produto() {
-            Quantidade = 0;
+            _quantidade = 0;
         }
 
         public Produto(string nome, double preco) : this() {
-            Nome = nome;
-            Preco = preco;
+            _nome = nome;
+            _preco = preco;
         }
 
-        public Produto(string nome, double preco, int quantidade) : this(nome,preco) {
-            Quantidade= quantidade;
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) {
+            _quantidade = quantidade;
+        }
+
+        public string GetNome() {
+            return this._nome;
+        }
+
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1) {
+                this._nome = nome;
+            }
+
+        }
+
+        public double GetPreco() {
+            return this._preco;
+        }
+
+        public int GetQuantidade() {
+            return this._quantidade;
         }
 
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString() {
-            return Nome
+            return _nome
             + ", $ "
-            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
             + ", "
-            + Quantidade
+            + _quantidade
             + " unidades, Total: $ "
             + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
--- Course mod5/Program.cs
+++ Course mod5/Program.cs
@@ -4,39 +4,11 @@ namespace Course_mod5 {
     class Program {
         static void Main(string[] args) {
 
+            Produto p = new Produto("TV", 500.00, 10);
 
-            Console.WriteLine("Entre os dados do produto:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.SetNome("T");
 
-            Produto p = new Produto(nome, preco);
-
-            Produto p2 = new Produto();
-
-            Produto p3 = new Produto() {
-                Nome = "Tv", 
-                Preco = 500.00, 
-                Quantidade = 20 
-            };
-
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
+            Console.WriteLine(p);
         }
     }
 }
